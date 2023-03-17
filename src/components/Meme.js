dadba3899ec3d10a2cc540217bd7f59280d277af
Ana Beatriz Adriano da Silva <ana.github0@gmail.com>
2023-03-17 11:21:08 -0300

    .

---
+++ src/components/Meme.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <form className="form">
+      <div>
+        <input type="text" />
+        <input type="text" />
+      </div>
+
+      <button>Get a new meme image</button>
+    </form>
+  )
+}
